fb3c2c9a08fdae47471cb2b19f64778babe8a058
yamakamisaki <s.chuuu02@gmail.com>
2022-06-21 11:21:15 +0900

    メソッドの練習

---
+++ js/2.js
@@ -0,0 +1,14 @@
+$(function(){
+  $('#btn_2').on('click',()=>{
+    $('#push').hide();
+  });
+
+
+  $('#sky').fadeIn('slow');
+  $('#sky2').slideDown('slow');
+
+  $('#sky3').fadeOut('slow');
+  $('#sky4').slideUp('slow');
+
+
+});

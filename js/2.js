a61f0b29d566a9f7ea29f38975661fbf580787d9
yamakamisaki <s.chuuu02@gmail.com>
2022-06-22 22:22:24 +0900

    CSSメソッドの学習

--- js/2.js
+++ js/2.js
@@ -1,14 +1,37 @@
 $(function(){
+
+  //hideメソッド使用
   $('#btn_2').on('click',()=>{
     $('#push').hide();
   });
 
+  // CSSメソッド使用
+  $('#btn_3').on('click',()=>{
+    $('#push2').css({
+      fontSize: '30px',
+      color: 'red'
+    });
+  });
 
+  $('#btn_4').on('click',()=>{
+    $('#push2').css({
+      fontSize: '9px',
+      color: 'blue'
+    });
+  });
+
+  $('#btn_5').on('click',()=>{
+    $('#push2').css({
+      fontSize: '17px',
+      color: 'orange'
+    });
+  });
+
+  // 表示方法を変えるメソッド使用
   $('#sky').fadeIn('slow');
   $('#sky2').slideDown('slow');
 
   $('#sky3').fadeOut('slow');
   $('#sky4').slideUp('slow');
-
 
 });
